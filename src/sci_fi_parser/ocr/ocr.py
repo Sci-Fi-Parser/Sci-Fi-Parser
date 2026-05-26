66779e14ffed84426ba4fa18bef0908771205cc5
Niko Typpö <niko.typpo@protonmail.com>
2026-05-26 17:44:43 +0300

    begin ocr implementation

---
+++ src/sci_fi_parser/ocr/ocr.py
@@ -0,0 +1,35 @@
+import json
+
+from paddleocr import PaddleOCR
+
+# PNG/JPG --> labels, values, confidence score.
+
+class Ocr:
+    def __init__(self, input_path: str):
+        self.input_path = input_path.strip()
+
+    def run_ocr(self) -> dict:
+        ocr = PaddleOCR(
+            use_doc_orientation_classify=False, # Disables document orientation classification model via this parameter
+            use_doc_unwarping=False, # Disables text image rectification model via this parameter
+            use_textline_orientation=False, # Disables text line orientation classification model via this parameter
+            lang="en",
+        )
+
+        result = ocr.predict(self.input_path)
+        extracted = {}
+        for res in result:
+            # res.save_to_img("output")  
+            # res.save_to_json("output")
+            extracted["labels"] = res.get("rec_texts")
+            extracted["confidence"] = res.get("rec_scores")
+
+        return extracted
+
+
+if __name__ == "__main__":
+    input_path = input("Enter path for input: ")
+    ocr = Ocr(input_path)
+    result = ocr.run_ocr()
+    json_result = json.dumps(result, indent=4)
+    print(json_result)
