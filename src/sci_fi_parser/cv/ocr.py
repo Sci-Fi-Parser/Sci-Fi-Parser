382a225ad1ec5f47e9b827a855e2e61e41939838
Niko Typpö <niko.typpo@protonmail.com>
2026-05-27 14:27:43 +0300

    Refactor PaddleOCR startup

--- src/sci_fi_parser/cv/ocr.py
+++ src/sci_fi_parser/cv/ocr.py
@@ -5,18 +5,22 @@ from paddleocr import PaddleOCR
 # PNG/JPG --> labels, values, confidence score.
 
 class Ocr:
-    def __init__(self, input_path: str):
-        self.input_path = input_path
+    def __init__(self, input_path=None):
+        if input_path:
+            self.input_path = input_path
 
-    def run_ocr(self) -> str:
-        ocr = PaddleOCR(
+        self.ocr = PaddleOCR(
             use_doc_orientation_classify=False, # Disables document orientation classification model via this parameter
             use_doc_unwarping=False, # Disables text image rectification model via this parameter
             use_textline_orientation=False, # Disables text line orientation classification model via this parameter
             lang="en",
         )
 
-        result = ocr.predict(self.input_path)
+    def read_image(self, input_path) -> None:
+        self.input_path = input_path
+
+    def run_ocr(self) -> str:
+        result = self.ocr.predict(self.input_path)
         extracted = {}
         for res in result:
             # res.save_to_img("output")  
--- src/sci_fi_parser/cv/pipeline.py
+++ src/sci_fi_parser/cv/pipeline.py
@@ -8,13 +8,13 @@ import cv2
 def start_ocr(folder: str) -> list:
     path = Path(folder).glob("*.jpg")
     result = []
+    ocr = Ocr()
     for image in path:
-        print(image)
         image_array = cv2.imread(image)
         bar_candidates = bars.detect_bars(image_array)
         result.append(bar_candidates)
 
-        ocr = Ocr(image_array)
+        ocr.read_image(image_array)
         ocr_json = ocr.run_ocr()
         result.append(ocr_json)
 
