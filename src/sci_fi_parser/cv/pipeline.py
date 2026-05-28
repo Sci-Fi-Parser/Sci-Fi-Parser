1d1a3f881744ed354aee9251b1256d5d4f32ff49
Niko Typpö <niko.typpo@protonmail.com>
2026-05-28 14:15:08 +0300

    Refactor OCR pipeline for main

--- src/sci_fi_parser/cv/pipeline.py
+++ src/sci_fi_parser/cv/pipeline.py
@@ -1,25 +1,25 @@
 from pathlib import Path
 
 from ocr import Ocr
+
 import bars
 
 import cv2
 
-def start_ocr(folder: str, ocr_set: dict) -> dict:
+def start_ocr(folder: str, ocr_set: dict):
     path = Path(folder).glob("*.jpg")
-    result = {}
     ocr = Ocr()
-    for i, image in enumerate(path):
+    for image in path:
         image_array = cv2.imread(image)
         bar_candidates = bars.detect_bars(image_array)
 
         ocr.read_image(image_array)
-        ocr_json = ocr.run_ocr()
+        ocr_res = ocr.run_ocr()
+        ocr_set.add(image , (bar_candidates, ocr_res))
 
-        result[i] = bar_candidates, ocr_json
-
-    return result
 
 if __name__ == "__main__":
     input_path = input("Enter input path: ")
-    print(start_ocr(input_path, {}))
+    ocr_set = OCRSet()
+    start_ocr(input_path, ocr_set)
+    print(ocr_set)
