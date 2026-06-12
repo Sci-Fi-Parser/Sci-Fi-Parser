0ea810fefeaf84f4b5b6f12c384213421c0896ba
Antti Moisio <moisioa1@gmail.com>
2026-06-12 13:53:17 +0300

    Added methods to create dummy model and transform when ImageClassifier is called without parameters

--- src/sci_fi_parser/classifier/image_classifier.py
+++ src/sci_fi_parser/classifier/image_classifier.py
@@ -5,9 +5,15 @@ from pathlib import Path
 import torch
 
 class ImageClassifier:
-    def __init__(self, model: CNNClassifier, image_transform):
-        self.transform = image_transform
-        self.model = model
+    def __init__(self, model: CNNClassifier = None, image_transform = None):
+        if model is None:
+            self.model = ImageClassifier.create_dummy_model()
+        else:
+            self.model = model
+        if image_transform is None:
+            self.transform = ImageClassifier.create_dummy_tranform()
+        else:
+            self.transform = image_transform
 
     def classify_image(self, image):
         with torch.no_grad(): #with a trained network, gradient computation is not needed
@@ -15,13 +21,20 @@ class ImageClassifier:
                 as_tensor = torch.unsqueeze(self.transform(im), 0) #A dummy batch dimension is added to the tensor
                 model_output = self.model.forward(as_tensor)
                 return torch.argmax(model_output), model_output
+            
+    @classmethod
+    def create_dummy_model(cls):
+        input_size = 128
+        return CNNClassifier(num_classes=6, channels_in=3, channels_out=[32, 64, 128], 
+                          conv_kernel_size=3, pool_size=2, linear_layer_neurons=256, input_image_size=input_size)
 
+    @classmethod
+    def create_dummy_tranform(cls):
+        input_size = 128
+        return transforms.Compose([transforms.Resize((input_size, input_size)), transforms.ToTensor()])
         
 if __name__=="__main__":
-    input_size = 128
-    model = CNNClassifier(num_classes=6, channels_in=3, channels_out=[32, 64, 128], 
-                          conv_kernel_size=3, pool_size=2, linear_layer_neurons=256, input_image_size=input_size)
-    im_transform = transforms.Compose([transforms.Resize((input_size, input_size)), transforms.ToTensor()])
-    im_classifier = ImageClassifier(model=model, image_transform=im_transform)
+    im_classifier = ImageClassifier()
     my_image = Path("cnn_data_split/val/graphs_val/0a14bb795a27.jpg")
     classification, full_output = im_classifier.classify_image(my_image)
+    print(classification, full_output)
