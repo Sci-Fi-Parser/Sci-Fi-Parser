ec96b6e2627a841bbf619e0491da08fc7c314347
Niko Typpö <niko.typpo@protonmail.com>
2026-07-22 13:12:14 +0300

    Change image UID to hash

--- src/sci_fi_parser/image_extraction/image_parser.py
+++ src/sci_fi_parser/image_extraction/image_parser.py
@@ -11,15 +11,16 @@ from pathlib import Path
 from typing import cast
 from uuid import uuid4
 
+import imagehash
 import pymupdf
 from PIL import Image
 
 MAX_IMAGE_SIZE = 1000
 
 
-def create_image_id() -> str:
-    """Create an image id shared by extracted and input images."""
-    return str(uuid4())
+def _create_hash(img: Image.Image) -> str:
+    """Create an image hash shared by extracted and input images."""
+    return str(imagehash.average_hash(img))
 
 
 def start_parser(
@@ -82,8 +83,8 @@ def extract_images(
                     continue
 
                 if downsized_img:
-                    image_id = create_image_id()
-                    image_data[image_id] = (
+                    image_hash = _create_hash(downsized_img)
+                    image_data[image_hash] = (
                         downsized_img,
                         {
                             "pdf_id": pdf_id,
@@ -117,8 +118,8 @@ def extract_drawings(
                 continue
 
             if img:
-                image_id = create_image_id()
-                image_data[image_id] = (
+                image_hash = _create_hash(img)
+                image_data[image_hash] = (
                     img,
                     {
                         "pdf_id": pdf_id,
